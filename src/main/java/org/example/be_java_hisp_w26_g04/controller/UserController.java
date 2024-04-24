37adf2f93f27da8a9e9a9fbd59d38b34ccf57471
valentin-aquino <valentin.aquino@mercadolibre.com>
2024-04-24 10:21:04 -0300

    crear endpoint get followers

--- src/main/java/org/example/be_java_hisp_w26_g04/controller/UserController.java
+++ src/main/java/org/example/be_java_hisp_w26_g04/controller/UserController.java
@@ -1,9 +1,11 @@
 package org.example.be_java_hisp_w26_g04.controller;
 
+import org.example.be_java_hisp_w26_g04.dto.SellerFollowersDto;
 import org.example.be_java_hisp_w26_g04.service.buyer.IBuyerService;
+import org.example.be_java_hisp_w26_g04.service.seller.ISellerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-    @Autowired
-    IBuyerService buyerService;
+  @Autowired
+  IBuyerService buyerService;
 
-    @PostMapping("/{userId}/follow/{userIdToFollow}")
-    public ResponseEntity<?> follow(@PathVariable int userId, @PathVariable int userIdToFollow) {
-        buyerService.followSeller(userId, userIdToFollow);
-        return ResponseEntity.ok().build();
-    }
+  @Autowired
+  ISellerService sellerService;
 
+  @PostMapping("/{userId}/follow/{userIdToFollow}")
+  public ResponseEntity<?> follow(@PathVariable int userId, @PathVariable int userIdToFollow) {
+    buyerService.followSeller(userId, userIdToFollow);
+    return ResponseEntity.ok().build();
+  }
+
+  @GetMapping("/{userId}/followers/list")
+  public ResponseEntity<SellerFollowersDto> getFollowers(@PathVariable int userId) {
+    return ResponseEntity.ok().body(sellerService.getFollowers(userId));
+  }
 }
--- src/main/java/org/example/be_java_hisp_w26_g04/service/seller/SellerService.java
+++ src/main/java/org/example/be_java_hisp_w26_g04/service/seller/SellerService.java
@@ -1,4 +1,33 @@
 package org.example.be_java_hisp_w26_g04.service.seller;
 
-public class SellerService {
+import java.util.List;
+import org.example.be_java_hisp_w26_g04.dto.SellerFollowersDto;
+import org.example.be_java_hisp_w26_g04.dto.UserDto;
+import org.example.be_java_hisp_w26_g04.model.Seller;
+import org.example.be_java_hisp_w26_g04.repository.seller.ISellerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SellerService implements ISellerService {
+
+  @Autowired
+  ISellerRepository sellerRepository;
+
+  @Override
+  public SellerFollowersDto getFollowers(int userId) {
+    // TODO: Create a coherent Exception to be thrown here
+    Seller seller = sellerRepository.findById(userId).orElseThrow(RuntimeException::new);
+
+    return converSellerToSellerFollowersDto(seller);
+  }
+
+  private SellerFollowersDto converSellerToSellerFollowersDto(Seller seller) {
+    List<UserDto> followers = seller.getListFollowers().stream()
+        .map(follower -> new UserDto(follower.getUserId(), follower.getUserName()))
+        .toList();
+
+    return new SellerFollowersDto(seller.getUserId(), seller.getUserName(), followers);
+  }
+
 }
