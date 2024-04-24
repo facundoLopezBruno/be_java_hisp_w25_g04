d3444eefbd398c1e2f1e06e394a002e3319f8560
FacumpMELI <facundosebastian.molinaperera@mercadolibre.com>
2024-04-24 09:41:14 -0300

    Lista de vendedores de un usuario

--- src/main/java/org/example/be_java_hisp_w26_g04/controller/UserController.java
+++ src/main/java/org/example/be_java_hisp_w26_g04/controller/UserController.java
@@ -1,13 +1,13 @@
 package org.example.be_java_hisp_w26_g04.controller;
 
+import org.example.be_java_hisp_w26_g04.model.Buyer;
 import org.example.be_java_hisp_w26_g04.service.buyer.IBuyerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -20,6 +20,17 @@ public class UserController {
     public ResponseEntity<?> follow(@PathVariable int userId, @PathVariable int userIdToFollow) {
         buyerService.followSeller(userId, userIdToFollow);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{userId}/followed/list")
+    public ResponseEntity<?> getSellerList(@PathVariable int userId){
+        Optional<Buyer> buyer= buyerService.findById(userId);
+        if(buyer.isPresent()){
+            return new ResponseEntity<>(buyer.get(), HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>("No se encuentra el usuario con id: "+userId, HttpStatus.NOT_FOUND);//return ResponseEntity.badRequest().build();
+        }
     }
 
 }
