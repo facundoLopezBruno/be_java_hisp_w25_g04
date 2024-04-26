90530269f8b4a5805c9c384da160b887b4ee52e4
Edgardo Molina <molina.edgardo.2983@gmail.com>
2024-04-26 16:02:08 -0300

    se corrige un problema en el DTO que no mostraba correctamente los valores

--- src/main/java/org/example/be_java_hisp_w26_g04/dto/PromoCountResponseDTO.java
+++ src/main/java/org/example/be_java_hisp_w26_g04/dto/PromoCountResponseDTO.java
@@ -1,5 +1,6 @@
 package org.example.be_java_hisp_w26_g04.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,8 +11,10 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PromoCountResponseDTO {
+    @JsonAlias("userId")
     @JsonProperty("user_id")
     private int userId;
+    @JsonAlias("userName")
     @JsonProperty("user_name")
     private String userName;
     @JsonProperty("promo_products_count")
