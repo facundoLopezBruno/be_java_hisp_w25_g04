070f150f2e8a4f8542265ecab5a9c3f8a32ec28d
Emiliano Corvalan <jorge.corvalan@mercadolibre.com>
2024-05-06 15:58:42 -0300

    Se corrigen validaciones y excepciones

--- src/main/java/org/example/be_java_hisp_w26_g04/exceptions/GlobalExceptionHandler.java
+++ src/main/java/org/example/be_java_hisp_w26_g04/exceptions/GlobalExceptionHandler.java
@@ -3,9 +3,17 @@ package org.example.be_java_hisp_w26_g04.exceptions;
 import org.example.be_java_hisp_w26_g04.dto.BadResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice(annotations = RestController.class)
 public class GlobalExceptionHandler {
@@ -21,5 +29,14 @@ public class GlobalExceptionHandler {
     );
 
     return new ResponseEntity<>(badResponseDto, HttpStatus.NOT_FOUND);
+  }
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    Map<String, String> errorMap = new HashMap<>();
+    ex.getBindingResult().getFieldErrors().forEach(error -> {
+      errorMap.put(error.getField(), error.getDefaultMessage());
+    });
+
+    return ResponseEntity.badRequest().body(errorMap);
   }
 }
