7a835f07d76309487db6e382b755e5369f0b4085
facundoLopezBruno <facundo.lopezbruno@mercadolibre.com>
2024-05-06 12:38:59 -0300

    agrego test vacios service buyer y seller

---
+++ src/test/java/org/example/be_java_hisp_w26_g04/service/buyer/BuyerServiceTest.java
@@ -0,0 +1,24 @@
+package org.example.be_java_hisp_w26_g04.service.buyer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BuyerServiceTest {
+
+    @Test
+    void followSeller() {
+    }
+
+    @Test
+    void getFollowed() {
+    }
+
+    @Test
+    void unfollowerSeller() {
+    }
+
+    @Test
+    void sortGetFollowed() {
+    }
+}
---
+++ src/test/java/org/example/be_java_hisp_w26_g04/service/seller/SellerServiceTest.java
@@ -0,0 +1,28 @@
+package org.example.be_java_hisp_w26_g04.service.seller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SellerServiceTest {
+
+    @Test
+    void findFollowers() {
+    }
+
+    @Test
+    void getFollowers() {
+    }
+
+    @Test
+    void sortGetFollowers() {
+    }
+
+    @Test
+    void sortGetPostFromFollower() {
+    }
+
+    @Test
+    void createNewPost() {
+    }
+}
