f5a2f1331b882ab08410a039267f46cb3cd5e6f9
Emiliano Corvalan <jorge.corvalan@mercadolibre.com>
2024-05-06 17:54:04 -0300

    Se agrega DTO para errores de validacion

--- src/main/java/org/example/be_java_hisp_w26_g04/exceptions/GlobalExceptionHandler.java
+++ src/main/java/org/example/be_java_hisp_w26_g04/exceptions/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package org.example.be_java_hisp_w26_g04.exceptions;
 
 import org.example.be_java_hisp_w26_g04.dto.BadResponseDTO;
+import org.example.be_java_hisp_w26_g04.dto.ValidationErrorsResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,6 +17,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Validated
 @ControllerAdvice(annotations = RestController.class)
 public class GlobalExceptionHandler {
   @ExceptionHandler(BadRequestException.class)
@@ -30,13 +33,17 @@ public class GlobalExceptionHandler {
 
     return new ResponseEntity<>(badResponseDto, HttpStatus.NOT_FOUND);
   }
+
   @ExceptionHandler(MethodArgumentNotValidException.class)
-  public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+  public ResponseEntity<ValidationErrorsResponseDTO> handleValidationExceptions(MethodArgumentNotValidException ex) {
     Map<String, String> errorMap = new HashMap<>();
     ex.getBindingResult().getFieldErrors().forEach(error -> {
       errorMap.put(error.getField(), error.getDefaultMessage());
     });
 
-    return ResponseEntity.badRequest().body(errorMap);
+    ValidationErrorsResponseDTO errorsResponseDTO =
+            new ValidationErrorsResponseDTO("Hay errores de validación", errorMap);
+
+    return ResponseEntity.badRequest().body(errorsResponseDTO);
   }
 }
