65be147af6aea99fc33f455e0d7e0715f27323dd
Emiliano Corvalan <jorge.corvalan@mercadolibre.com>
2024-05-07 18:57:34 -0300

    Se agregan tests T-02 :) :(

--- src/test/java/org/example/be_java_hisp_w26_g04/service/buyer/BuyerServiceTest.java
+++ src/test/java/org/example/be_java_hisp_w26_g04/service/buyer/BuyerServiceTest.java
@@ -6,6 +6,7 @@ import org.example.be_java_hisp_w26_g04.model.Seller;
 import org.example.be_java_hisp_w26_g04.repository.buyer.IBuyersRepository;
 import org.example.be_java_hisp_w26_g04.repository.seller.ISellerRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -27,6 +28,7 @@ class BuyerServiceTest {
     private BuyerService buyerService;
 
     @Test
+    @DisplayName("T-0001 El usuario a seguir existe OK")
     void followSellerOkTest() {
         //Arrange
         int buyerId = 1;
@@ -54,14 +56,17 @@ class BuyerServiceTest {
     }
 
     @Test
+    @DisplayName("T-0001 El usuario a seguir NO existe")
     void followSellerNotOkTest(){
+        //Arrange
         int buyerId = 1;
         int sellerId = 9999;
 
         when(buyersRepository.findById(buyerId)).thenThrow(new BadRequestException());
 
-        Assertions.assertThrows(BadRequestException.class ,()->
-            buyerService.followSeller(buyerId,sellerId)
+        //Act & Assert
+        Assertions.assertThrows(BadRequestException.class,
+                                ()-> buyerService.followSeller(buyerId,sellerId)
         );
     }
 
@@ -70,7 +75,45 @@ class BuyerServiceTest {
     }
 
     @Test
-    void unfollowerSeller() {
+    @DisplayName("T-0002 El usuario a dejar de seguir existe")
+    void unfollowSellerOkTest() {
+        //Arrange
+        int buyerId = 1;
+        int sellerId = 2;
+
+        Buyer buyer = new Buyer();
+        buyer.setUserId(buyerId);
+        buyer.setUserName("Comprador");
+
+        Seller seller = new Seller();
+        seller.setUserId(sellerId);
+        seller.setUserName("Vendedor");
+
+        when(buyersRepository.findById(buyerId)).thenReturn(Optional.of(buyer));
+
+        when(sellerRepository.findById(sellerId)).thenReturn(Optional.of(seller));
+
+        buyer.getSellersFollowing().add(sellerId);
+        seller.getFollowers().add(buyerId);
+
+        buyerService.unfollowerSeller(buyerId,sellerId);
+
+        Assertions.assertFalse(buyer.getSellersFollowing().contains(sellerId));
+        Assertions.assertFalse(seller.getFollowers().contains(buyerId));
+    }
+
+    @Test
+    @DisplayName("T-0002 El usuario a dejar de seguir NO existe")
+    void unfollowSeller_UserNotExistsTest(){
+        int buyerId = 1;
+        int sellerId = 999;
+        Buyer buyer = new Buyer();
+        buyer.setUserId(buyerId);
+
+        when(buyersRepository.findById(buyerId)).thenThrow(new BadRequestException());
+
+        Assertions.assertThrows(BadRequestException.class,
+                                () -> buyerService.unfollowerSeller(buyerId, sellerId));
     }
 
     @Test
