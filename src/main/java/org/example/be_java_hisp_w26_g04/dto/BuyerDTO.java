b27a51bf5ddeafdbdddda0503888a58b871ac2d4
Edgardo2983 <molina.edgardo.2983@gmail.com>
2024-05-09 19:13:46 -0300

    se agregan mas test de integracion

--- src/main/java/org/example/be_java_hisp_w26_g04/dto/BuyerDTO.java
+++ src/main/java/org/example/be_java_hisp_w26_g04/dto/BuyerDTO.java
@@ -4,10 +4,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
-@Getter
+@Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class BuyerDTO {
